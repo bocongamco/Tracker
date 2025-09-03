e74fe0becc17c7dab4003c80beb3b89f1786dd85
Phat Cao <ncaovncountry@gmail.com>
2025-09-03 18:35:41 +1000

    Set up user and DailyTask models.

---
+++ Api/ProcessTracker/ProcessTracker/Models/DailyTask.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcessTracker.Models
+{
+    public class DailyTask
+    {
+        //PK
+        public int Id { get; set; }
+        //Foreign key to User
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public string TaskName { get; set; } = "";
+        public string? TaskNote { get; set; } = "";
+        public DateOnly TaskDueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public byte Priority { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CompletedAt { get; set; }
+
+
+    }
+}
---
+++ Api/ProcessTracker/ProcessTracker/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessTracker.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = "";
+        public string? PasswordHash { get; set; } = "";
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        //Navigate to DailyTasks, User Can have multiple DailyTasks
+        public ICollection<DailyTask> DailyTasks { get; set; }
+    }
+}
