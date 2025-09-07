83af1139f3171cd8401e54e4deff428d0d871ebc
Phat Cao <ncaovncountry@gmail.com>
2025-09-07 19:59:58 +1000

    Daily task

--- Api/ProcessTracker/ProcessTracker/Data/Configuration/DailyTaskConfiguration.cs
+++ Api/ProcessTracker/ProcessTracker/Data/Configuration/DailyTaskConfiguration.cs
@@ -8,7 +8,15 @@ namespace ProcessTracker.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<DailyTask> b)
         {
-            b.HasKey(b => b.Id);
+            b.HasKey(x => x.Id);
+            b.Property(x => x.TaskName).IsRequired().HasMaxLength(200);
+            b.Property(x => x.TaskNote).HasMaxLength(2000);
+
+            
+            b.Property(x => x.TaskDueDate).HasColumnType("date");
+
+            b.Property(x => x.Priority).HasDefaultValue((byte)2);
+            b.HasIndex(x => new { x.UserId, x.TaskDueDate }); 
         }
     }
 }
